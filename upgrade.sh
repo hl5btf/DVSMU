ac1e272562799dd4f75fffdf079bc9b368ca0db5
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-11-17 12:18:04 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#sudo wget https://raw.githubusercontent.com/hl5btf/DVSMU/main/dvsm.basic
-sudo wget -O ${DVS}dvsmu https://raw.githubusercontent.com/hl5btf/DVSMU/main/dvsmu_test
+sudo wget -O /usr/local/dvs/dvsmu https://raw.githubusercontent.com/hl5btf/DVSMU/main/dvsmu
