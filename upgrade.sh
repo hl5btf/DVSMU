757c037b0e06809f873624c51a20d671184f142a
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-11-20 21:54:58 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -6,14 +6,13 @@ sudo chmod +x /usr/local/dvs/dvsmu
 #----v.1.4.5에서 var.txt의 Description이 바뀌기 때문에 처리하는 내용-------------------------------------------------------
 sudo wget -O /var/lib/dvswitch/dvs/var00.txt https://raw.githubusercontent.com/hl5btf/DVSMU/main/var00.txt > /dev/null 2>&1
 
+TERM=ansi whiptail --title "$T029" --infobox "사용자설정이 있어서 시간이 걸립니다." 8 60
+
 user="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20"
 
 for user in $user; do
 source /var/lib/dvswitch/dvs/var${user}.txt > /dev/null 2>&1
 if [ -e /var/lib/dvswitch/dvs/var${user}.txt ] && [ x${call_sign} != x ]; then
-    clear
-    TERM=ansi whiptail --title "$T029" --infobox "user${user} 처리중" 8 60
-
     update_var pwr 0
     update_var hgt 0
     update_var desc dvsMU
