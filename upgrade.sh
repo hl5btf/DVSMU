cdff7399ed4341bf6984d95b8a93831b510e3cb5
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-11-20 21:51:24 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -11,7 +11,7 @@ user="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20"
 for user in $user; do
 source /var/lib/dvswitch/dvs/var${user}.txt > /dev/null 2>&1
 if [ -e /var/lib/dvswitch/dvs/var${user}.txt ] && [ x${call_sign} != x ]; then
-
+    clear
     TERM=ansi whiptail --title "$T029" --infobox "user${user} 처리중" 8 60
 
     update_var pwr 0
