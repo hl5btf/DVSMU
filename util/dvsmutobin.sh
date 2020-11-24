9e6810d48e0e9e218ef18752234ed579d01e2922
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-11-24 18:25:22 +0900

    Update dvsmutobin.sh

--- util/dvsmutobin.sh
+++ util/dvsmutobin.sh
@@ -13,3 +13,8 @@ sudo \cp -f ${DVS}dvsmu.sh ${HOME}dvsmu.sh
 
 ${SHC}shc -r -v -T -f ${HOME}dvsmu.sh
 sudo mv ${HOME}dvsmu.sh.x ${HOME}dvsmu
+
+
+# 아래와 같이 다운로드가 가능함
+# wget -O /usr/local/dvs/dvsmutobin.sh https://raw.githubusercontent.com/hl5btf/DVSMU/main/util/dvsmutobin.sh
+# chmod +x /usr/local/dvs/dvsmutobin.sh
