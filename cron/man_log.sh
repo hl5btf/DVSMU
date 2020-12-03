0a15974c5b84f3364bb44adba914de5b8f16467c
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-12-03 12:48:18 +0900

    Add files via upload

--- cron/man_log.sh
+++ cron/man_log.sh
@@ -4,9 +4,9 @@
 user="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20"
 
 for user in $user; do
-	log_dir=/home/dvswitch/user${user}
+	log_dir=/var/log/dvswitch/user${user}
 	if [ -d $log_dir ]; then
-		file=/home/dvswitch/user${user}/Analog_Bridge.log
+		file=/var/log/dvswitch/user${user}/Analog_Bridge.log
 		date=$(date -d '4 day ago' '+%Y-%m-%d')
 		row_no=$(grep -n $date $file | cut -d: -f1 | head -1)
 		sudo sed -i "1,${row_no}d" $file
@@ -15,9 +15,9 @@ done
 
 
 log_dir=/var/log/dvswitch
-sudo find ${log_dir}/*.log -mtime +4 -exec rm -f {} \;
+sudo find $log_dir -name '*.log' -mtime +4 -delete
 
-log_dir=/var/log/mmdvm
-sudo find ${log_dir}/*.log -mtime +4 -exec rm -f {} \;
 
+log_dir= /var/log/mmdvm
+sudo find $log_dir -name '*.log' -mtime +4 -delete
 
