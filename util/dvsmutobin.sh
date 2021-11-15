90655a4f9094cf43496cc1cfb6dde823bd8b3415
hl5ky <64560125+hl5ky@users.noreply.github.com>
2021-11-15 21:33:02 +0900

    Update dvsmutobin.sh

--- util/dvsmutobin.sh
+++ util/dvsmutobin.sh
@@ -15,6 +15,20 @@ ${SHC}shc -r -v -T -f ${HOME}dvsmu.sh
 sudo mv ${HOME}dvsmu.sh.x ${HOME}dvsmu
 
 
-# 아래와 같이 다운로드가 가능함
+# 아래와 같이 다운로드가 가능함 =====================================
 # sudo wget -O /usr/local/dvs/dvsmutobin.sh https://raw.githubusercontent.com/hl5btf/DVSMU/main/util/dvsmutobin.sh
 # sudo chmod +x /usr/local/dvs/dvsmutobin.sh
+
+
+# shc 설치 ========================================================
+# wget http://www.datsi.fi.upm.es/~frosal/sources/shc-3.8.9.tgz
+# tar xvfz shc-3.8.9.tgz
+#cd shc-3.8.9
+# make (필히 실행해야 함)
+
+# 바이너리화:
+# ./shc -r -v -T -f ./파일명
+
+# 본래의 스크립트 파일하나,
+# 바이너리파일인 .x
+# 그리고 쉘스크립트가 c코드로 변환되었던 .c코드가 만들어진다.
