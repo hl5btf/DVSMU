45245c6149d5373a5f2681590f56449f3d128432
hl5ky <64560125+hl5ky@users.noreply.github.com>
2021-11-22 10:19:48 +0900

    Update dvsmutobin.sh

--- util/dvsmutobin.sh
+++ util/dvsmutobin.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 # dvsmu를 만들고 나서, Binary로 바꾸고 Backup을 만드는 등의 작업을 자동화 함.
+# shc 프로그램을 설치한다 (맨 아래 설명 있음)
+# 텍스트로 된 dvsmu를 바이너리 파일로 만드는 작업이다.
+# 한 단계씩 실행하므로 각 단계마다 엔터를 누른다.
 
 DVS=/usr/local/dvs/
 HOME=/home/dvswitch/
 SHC=/home/dvswitch/shc-3.8.9/
 
 sudo \cp -f ${DVS}dvsmu ${DVS}dvsmu.bak
+echo "copied to bak"
+read
 sudo \cp -f ${DVS}dvsmu ${DVS}dvsmu.sh
-
-sudo \cp -f ${DVS}dvsmu.sh ${HOME}dvsmu.sh
-
-${SHC}shc -r -v -T -f ${HOME}dvsmu.sh
-sudo mv ${HOME}dvsmu.sh.x ${HOME}dvsmu
+echo "copied to sh"
+read
+sudo \cp -f ${DVS}dvsmu ${HOME}dvsmu
+echo "copied to HOME"
+read
+${SHC}shc -r -v -T -f ${HOME}dvsmu
+echo "shrinked"
+read
+sudo mv ${HOME}dvsmu.x ${HOME}dvsmu
+echo "copied the file x to dvsmu"
+read
 sudo \cp -f ${HOME}dvsmu ${DVS}dvsmu
+echo "copied to DVS folder"
 
 
 # 아래와 같이 다운로드가 가능함 =====================================
