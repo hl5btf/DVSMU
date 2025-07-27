a4832d516c4f2b291ac1d63c932696b76d79a5b6
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-07-27 14:42:42 +0900

    Create upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+file=/usr/local/dvs/dvsmu_upgrade.sh
+sudo wget -O $file https://raw.githubusercontent.com/hl5btf/DVSMU/main/dvsmu_upgrade.sh > /dev/null 2>&1
+sudo chmod +x $file
+sudo $file
+sudo rm $file
+
+exit 1
