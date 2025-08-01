d167ad7d32f442631417b57c040f9f654581e28b
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-08-01 22:09:21 +0900

    Update auto_upgrade.sh

--- auto_upgrade.sh
+++ auto_upgrade.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# /usr/local/dvs/auto_upgrade.sh
 #===================================
 SCRIPT_VERSION="1.0"
 SCRIPT_AUTHOR="HL5KY"
 SCRIPT_DATE="2025-07-27"
 #===================================
+
+# 외부스크립트에서 auto_upgrade.sh를 호출할때 로그기록이 필요없다면, sudo env DISABLE_LOG=1 /usr/local/dvs/auto_upgrade.sh
 
 # dvsmu에서 필요한 함수만 불러오기
 dvsmu_file="/usr/local/dvs/dvsmu"
