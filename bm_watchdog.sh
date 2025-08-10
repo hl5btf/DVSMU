97ed74555c628703746f57879770e2367e18c1b2
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-08-10 11:13:47 +0900

    Update bm_watchdog.sh

--- bm_watchdog.sh
+++ bm_watchdog.sh
@@ -263,7 +263,7 @@ for test_bm_address in "${now_bm_address[@]}"; do
 						source /var/lib/dvswitch/dvs/var.txt
 
 						if [ "$call_sign" = "HL5KY" ]; then
-    						echo -e "$TIME\nvar${user}.txt\n초기 주소: $bm_address\n변경된 주소: $new_address" \
+    						echo -e "$TIME\nUSER${user}\n초기 주소: $bm_address\n변경된 주소: $new_address" \
     						| mail -s "bm_watchdog - 마스터서버 장애 감지 및 서버 변경" "$email"
 							# 다음의 두 가지 조건에 맞으면 email 전송
 							# 주사용자에 설정한 마스터서버가 작동되지 않아서 서버를 변경한 경우
@@ -387,7 +387,7 @@ for test_bm_address in "${original_bm_address[@]}"; do
 						source /var/lib/dvswitch/dvs/var.txt
 
 						if [ "$call_sign" = "HL5KY" ]; then
-                		echo -e "$TIME\nUSER${user}.txt\변경전 주소: $bm_address\n변경된 주소: $new_address" \
+                		echo -e "$TIME\nUSER${user}\변경전 주소: $bm_address\n변경된 주소: $new_address" \
 	                	| mail -s "bm_watchdog - 원래의 마스터서버 복구 및 서버 주소 변경" "$email"
 	        	        # 다음의 두 가지 조건에 맞으면 email 전송
         	        	# 주사용자에 설정한 마스터서버가 작동되지 않아서 서버를 변경한 경우
