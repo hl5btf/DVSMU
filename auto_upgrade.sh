6d9477c3e9a5d5ac2474be7b874bb1b7b94038b0
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-08-24 21:25:48 +0900

    Update auto_upgrade.sh

--- auto_upgrade.sh
+++ auto_upgrade.sh
@@ -51,6 +51,7 @@ if dpkg -l | grep -q "^ii  dvswitch-server" && apt-get -s upgrade | grep -q "^In
 	user_array="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"
 
 	for user in $user_array; do
+ 
 		source /var/lib/dvswitch/dvs/var${user}.txt > /dev/null 2>&1
   		if [ -e /var/lib/dvswitch/dvs/var${user}.txt ] && [ x${call_sign} != x ]; then
     		sudo systemctl stop mmdvm_bridge${user} analog_bridge${user} md380-emu${user} > /dev/null 2>&1
