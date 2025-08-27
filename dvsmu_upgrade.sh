c5657ea4e8e1877848cbc6fc614087ed4e26d636
hl5ky <64560125+hl5ky@users.noreply.github.com>
2025-08-27 21:24:04 +0900

    Update dvsmu_upgrade.sh

--- dvsmu_upgrade.sh
+++ dvsmu_upgrade.sh
@@ -276,7 +276,7 @@ echo "33 3 * * * root flock -n /var/lock/auto_upgrade.lock /usr/local/dvs/auto_u
 # CR/LF 제거 및 숫자만 있는 라인 제거
 sleep 0.2
 sudo sed -i 's/\r$//' /etc/crontab
-sudo sed -i -E '/^[[:space:]]*([0-9]+|[0-9]+[[:space:]]+[0-9]+)[[:space:]]*$/d' /etc/crontab
+sudo sed -i -E '/^[[:space:]]*[0-9]+([[:space:]]+[0-9]+)*[[:space:]]*(#[^!]*)?$/d' /etc/crontab
 }
 
 #====== add_variables =============================================
